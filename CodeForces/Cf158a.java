3b3c85e1829d1013c8b92620840ea6b1cbd4fec0
Abdulaminxon Thinkland <abdulaminxon@thinkland.uz>
2021-03-18 20:20:07 +0500

    158A

---
+++ CodeForces/Cf158a.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cf158a {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, k, count = 0;
+        n = scanner.nextInt();
+        k = scanner.nextInt();
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+        for (int i = 0; i < n; i++) {
+            if (list.get(i) > 0 && list.get(i) >= list.get(k - 1))
+                count++;
+        }
+        System.out.println(count);
+    }
+}
