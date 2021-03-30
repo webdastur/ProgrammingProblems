257d5715abaf3d6d193708f2ebd91698c5ea7552
Abdulaminxon <webdasturuz@gmail.com>
2021-03-30 12:38:41 +0500

    CF 58 A. Chat room

---
+++ CodeForces/CF58A.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+// A. Chat room
+public class CF58A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> s = new ArrayList<>(Arrays.asList(scanner.next().split("")));
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("hello".split("")));
+        StringBuilder result = new StringBuilder();
+        for (String value : s) {
+            if (words.isEmpty())
+                break;
+            if (words.get(0).equals(value)) {
+                words.remove(value);
+                result.append(value);
+            }
+        }
+        System.out.println(result.toString().equals("hello") ? "YES" : "NO");
+    }
+}
