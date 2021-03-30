0760889df64263e1328d0cc4a7ebc697d81a09b7
Abdulaminxon <webdasturuz@gmail.com>
2021-03-30 11:38:34 +0500

    CF 116 A. Tram

---
+++ CodeForces/CF116A.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+// A. Tram
+public class CF116A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short numberOfStops = scanner.nextShort();
+        int maxCapacity = 0;
+        int sum = 0;
+        for (int i = 0; i < numberOfStops; i++) {
+            short exits, enters;
+            exits = scanner.nextShort();
+            enters = scanner.nextShort();
+            sum -= exits;
+            sum += enters;
+            if (maxCapacity < sum)
+                maxCapacity = sum;
+        }
+        System.out.println(maxCapacity);
+    }
+}
