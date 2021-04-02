bf6d81b75c244d6180df51908ffb5b0f2d7547a7
Abdulaminxon <webdasturuz@gmail.com>
2021-04-02 18:21:50 +0500

    CF 677 A. Vanya and Fence

---
+++ CodeForces/CF677A.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+// A. Vanya and Fence
+public class CF677A {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        short n, h, width = 0;
+        n = scanner.nextShort();
+        h = scanner.nextShort();
+        for (int i = 0; i < n; i++) {
+            short temp = scanner.nextShort();
+            if (temp < h) {
+                width += 1;
+            } else if (temp > h) {
+                width += 2;
+            } else {
+                width += 1;
+            }
+        }
+        System.out.println(width);
+    }
+}
