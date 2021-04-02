4a7b6ce471ebf5259b11436330569e97076b42c0
Abdulaminxon <webdasturuz@gmail.com>
2021-04-02 20:37:25 +0500

    Masala #0002

---
+++ RoboContest/M0002.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+// Katta-kichik
+public class M0002 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b;
+        a = input.nextInt();
+        b = input.nextInt();
+        if (a > b)
+            System.out.println(">");
+        else if (a < b)
+            System.out.println("<");
+        else System.out.println("=");
+    }
+}
