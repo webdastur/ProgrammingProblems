fa2682b844e9209f69b63cad5147e7e95a2ca29c
Abdulaminxon <webdasturuz@gmail.com>
2021-04-02 20:28:43 +0500

    Masala #0001

---
+++ RoboContest/M0001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// A+B
+class M0001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a, b;
+        a = scanner.nextInt();
+        b = scanner.nextInt();
+        System.out.println(a + b);
+    }
+}
