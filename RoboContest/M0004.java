d37eac330fa9fc92e1500488b15c4cbfb3d1a855
Abdulaminxon <webdasturuz@gmail.com>
2021-04-02 20:55:40 +0500

    Masala #0004

---
+++ RoboContest/M0004.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+// Direktor tashrifi
+public class M0004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        long a, b;
+        a = input.nextLong();
+        b = input.nextLong();
+        System.out.println(a * b);
+    }
+}
