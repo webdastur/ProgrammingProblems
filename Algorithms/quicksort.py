d16d6b8b86004b468329fb270d1964a473dda22c
abdulaminxon-thinkland <abdulaminxon@thinkland.uz>
2021-11-22 18:02:51 +0500

    Algorithms: Quicksort added.

---
+++ Algorithms/quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(array: list) -> list:
+    if len(array) < 2:
+        return array
+    else:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([10, 5, 9, 7, 3, 8, 4, 1, 6, 2]))
