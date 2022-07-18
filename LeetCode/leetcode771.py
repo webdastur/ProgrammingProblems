ffbf3c951c8c9751919c7c7ff2a65bef6fc5afe6
webdastur <webdasturuz@gmail.com>
2022-07-18 15:00:03 +0500

    771. Jewels and Stones

---
+++ LeetCode/leetcode771.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/jewels-and-stones/
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        count = 0
+
+        for i in jewels:
+            count += stones.count(i)
+
+        return count
